1b9eff7256e98f42046bff28e83bcefa8efee638
Khawaja Hassan Mohiuddin <157839807+Khawaja-Hassan-Mohiuddin@users.noreply.github.com>
2024-04-02 17:12:00 +0500

    Add project files.

---
+++ Pages/AppDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NexCountStoreFront.Pages
+{
+    public class AppDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
