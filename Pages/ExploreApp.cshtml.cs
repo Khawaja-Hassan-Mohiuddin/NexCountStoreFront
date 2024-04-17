ac539730200056e9e50ed0463c3dfec1c4592c5f
Khawaja Hassan Mohiuddin <157839807+Khawaja-Hassan-Mohiuddin@users.noreply.github.com>
2024-04-18 00:41:42 +0500

    Created the responsive layout of the ExploreApp.cshtml page

---
+++ Pages/ExploreApp.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NexCountStoreFront.Pages
+{
+    public class ExploreAppModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
